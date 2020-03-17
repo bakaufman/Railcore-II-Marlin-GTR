3f0141cbcf3f721e7266f55d5a941f6a4cb7d41d
Brett <31897832+bakaufman@users.noreply.github.com>
2020-03-16 22:26:56 -0400

    Add files via upload
    updated with minor corrections

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -751,7 +751,7 @@
  * Override with M201
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_MAX_ACCELERATION      { 3000, 3000, 100, 10000 }
+#define DEFAULT_MAX_ACCELERATION      { 3000, 3000, 20, 1000 } //BAK based on railcore
 
 //#define LIMITED_MAX_ACCEL_EDITING     // Limit edit via M201 or LCD to DEFAULT_MAX_ACCELERATION * 2
 #if ENABLED(LIMITED_MAX_ACCEL_EDITING)
